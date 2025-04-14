fb9ae0c299cd093192a1361a1f85f0e40843602c
LukasHellanio <lukashellanio@gmail.com>
2025-04-14 19:22:25 -0300

    feat: setup Firebase with FlutterFire CLI and initialize FirebaseOptions
    - Installed Firebase CLI and FlutterFire CLI
    - Configured Firebase project with flutterfire configure
    - Generated firebase_options.dart with platform-specific configs
    - Updated main.dart to initialize Firebase using DefaultFirebaseOptions

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
+import 'package:encora_community/pages/login/encora_academy_login.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyB4J_hJbO6aasDV9wAwdwUXQLQ7I05z95M",
-      authDomain: "encora-community-app.firebaseapp.com",
-      projectId: "encora-community-app",
-      storageBucket: "encora-community-app.firebasestorage.app",
-      messagingSenderId: "752694225568",
-      appId: "1:752694225568:web:e34ee9d29106ef1ca2f7c5",
-    ),
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(
+    MaterialApp(debugShowCheckedModeBanner: false, home: EncoraAcademyLogin()),
   );
-  runApp(MaterialApp(home: EncoraAcademyLogin()));
 }
---
+++ lib/pages/login/encora_academy_login.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EncoraAcademyLogin extends StatelessWidget {
+  const EncoraAcademyLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
